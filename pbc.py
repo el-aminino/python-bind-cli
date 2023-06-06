27a3f6bb8fa4055b1dad4ac1e33d4baa3ee1d27f
el-aminino <aminmze81@gmail.com>
2023-06-06 20:30:26 +0330

    some updates

--- pbc.py
+++ pbc.py
@@ -5,6 +5,12 @@ c = subprocess.getoutput("apt list --installed")
 print("welcome to Bind9 editor")
 
 if "bind9" not in c:
+    bind_install = false
     print("Bind9 is not installed in this server please use command bellow as a privileged user \n")
     print("apt install bind9")
 
+else :
+    bind_install = true
+
+if bind_install :
+    print ("here is the zones in your configuration :")

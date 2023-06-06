5a0658ceba3c4e32a8571ec725a647fdfef237b5
el-aminino <aminmze81@gmail.com>
2023-06-06 20:36:06 +0330

    true to True and false to False

--- pbc.py
+++ pbc.py
@@ -5,12 +5,12 @@ c = subprocess.getoutput("apt list --installed")
 print("welcome to Bind9 editor")
 
 if "bind9" not in c:
-    bind_install = false
+    bind_install = False
     print("Bind9 is not installed in this server please use command bellow as a privileged user \n")
     print("apt install bind9")
 
 else :
-    bind_install = true
+    bind_install = True
 
 if bind_install :
     print ("here is the zones in your configuration :")
